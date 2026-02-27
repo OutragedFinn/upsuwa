283c138b189f9ce3b3f81df74d4f5f4fb3493fee
Zac <zachary.denboer@gmail.com>
2026-02-27 22:05:25 +0800

    add event page with some text

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar';
-import Footer from '../../components/footer/Footer'
-// import { Header } from '../Home'
+import Footer from '../../components/footer/Footer';
+import { Info } from '../Events';
 
 export default function Events() {
   return (
     <div className="Events">
       <Navbar />
+      <Info />
+      <Footer />
+      
     </div>
   )
 }
---
+++ src/pages/Events/info/Info.jsx
@@ -0,0 +1,18 @@
+
+import {useState, React, useEffect } from 'react'
+import "./info.css"
+
+export default function Info() {
+
+  return (
+    <div className='ups__home-info'>
+        <div className='ups__home-info_container'>
+            <div>
+                <h1>Events and activities</h1>
+                <p>UPS regularly runs numerous events, including ping-pong tournaments, study nights, star-gazing, physics demonstrations, guest lectures, and quiz nights. </p>
+                <p>If you are new to UWA, come find our stall</p>
+            </div>
+        </div>
+    </div>
+  )
+}
