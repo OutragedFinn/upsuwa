a514db406a0bea4b378ff92890a4978956ed7ba2
Zachary den Boer <zachary.denboer@gmail.com>
2026-03-07 17:19:49 +0800

    downscale photos
    images were 3,4k. down scaled to 1600px width
    kept ratio. improves load time, and also quality
    for some reason, something about sampling

--- src/pages/Home/photos/Photos.jsx
+++ src/pages/Home/photos/Photos.jsx
@@ -4,21 +4,25 @@ import "./photos.css"
 
 export default function Photos() {
 
+  const photoList = [
+    "/assets/demos1.jpg",
+    "/assets/demos2.jpg",
+    "/assets/demos4.jpg",
+    "/assets/astro1.jpg",
+    "/assets/demos3.jpg",
+    "/assets/tea1.jpg",
+  ]
+
   return (
     <div className='ups__home-photos'>
         <div className='ups__home-photos_container'>
            <div className="ups__home-photos_container-photogrid">
-                <div className="ups__home-photos_container-photogrid-photo"><img src="/assets/demos1.jpg" alt="Photo 1"/></div>
-                <div className="ups__home-photos_container-photogrid-photo"><img src="/assets/demos2.jpg" alt="Photo 2"/></div>
-                <div className="ups__home-photos_container-photogrid-photo"><img src="/assets/demos4.jpg" alt="Photo 3"/></div>
-                <div className="ups__home-photos_container-photogrid-photo"><img src="/assets/astro1.jpg" alt="Photo 4"/></div>
-                <div className="ups__home-photos_container-photogrid-photo"><img src="/assets/header.jpg" alt="Photo 5"/></div>
-                <div className="ups__home-photos_container-photogrid-photo"><img src="/assets/tea1.jpg" alt="Photo 6"/></div>
-                {/* <div className="photo"><img src="/assets/demos2.jpg" alt="Photo 2"/></div>
-                <div className="photo"><img src="/assets/photo3.jpg" alt="Photo 3"/></div>
-                <div className="photo"><img src="/assets/photo4.jpg" alt="Photo 4"/></div>
-                <div className="photo"><img src="/assets/photo5.jpg" alt="Photo 5"/></div>
-                <div className="photo"><img src="/assets/photo6.jpg" alt="Photo 6"/></div> */}
+                {photoList.map((pic) => (
+                  <div className='ups__home-photos_container-photogrid-photo'>
+                      <img src={pic} alt={pic.replace('/assets/', '')}/>
+                            
+                  </div>
+                ))}
             </div>
         </div>
     </div>
