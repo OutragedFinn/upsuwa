2a226b9bbaea563fffe5c2c0182d76af6550348b
Zac <zachary.denboer@gmail.com>
2026-03-11 17:03:49 +0800

    resources pg add kitchen para

--- src/pages/Home/info/Info.jsx
+++ src/pages/Home/info/Info.jsx
@@ -9,8 +9,8 @@ export default function Info() {
 
   return (
     <div className='ups__home-info'>
-        <div className='ups__home-info_container'>
-            <div>
+        <div>
+            <div className='ups__home-info_container'>
                 <h1>Welcome to the UWA University Physics Society</h1>
                 <p>The University Physics Society, est. 1965, represents all students studying or interested in Physics at the University of Western Australia (Guild affiliated).</p>
                 <p>We offer a wide range of social, educational and professional activities from Cocktail Nights and Ping Pong tournaments to Study Nights and Physics Demonstrations. Come check out our clubroom on the Physics building 2nd floor and follow our socials to stay in the loop on how you can get involved!</p>
--- src/pages/Resources/clubroom/Clubroom.jsx
+++ src/pages/Resources/clubroom/Clubroom.jsx
@@ -13,6 +13,8 @@ export default function Clubroom() {
             <h1>UPS Clubroom</h1>
             <Enlargeable_Image src='/assets/common-room1.webp' className='ups__resources-clubroom_photo' />
             <p>The Physics building (Room 2.19) hosts our common room. We have ping pong tables, a kitchen and ample study and lounge space for club members to enjoy.</p>
+            <h3>Kitchen</h3>
+            <p>Feel free to take advantage of our kitchenette, with a microwave, toaster, sandwich press and fridge space, everything you could need for your lunch. We also have a boiling water tap and supplies for coffee or tea.</p>
             <h3>Store</h3>
             <p>We run a zero-profit store, buying in bulk to supply students with the fuel to keep on studying, at the lowest prices possible. We regularly stock energy drinks, snack bars, frozen meals and more. Pay by cash or bank transfer.</p>
             
